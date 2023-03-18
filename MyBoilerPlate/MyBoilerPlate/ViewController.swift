08cf71832341037134f20d71241393fb02ccb7af
kth1210 <k2hop1210@gmail.com>
2023-03-18 23:16:06 +0900

    [ADD] : Project 파일 추가, 스토리보드 제거

---
+++ MyBoilerPlate/MyBoilerPlate/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyBoilerPlate
+//
+//  Created by 김태현 on 2023/03/18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .red
+    }
+
+
+}
+
